192ffdbd017fd4c9a671481338b35f25804c9642
valuesFeng <valuesFengS@gmail.com>
2015-05-16 17:29:39 +0800

    modified

--- app/src/main/java/com/yjrg/app/net/http/BasicRequest.java
+++ app/src/main/java/com/yjrg/app/net/http/BasicRequest.java
@@ -4,4 +4,13 @@ package com.yjrg.app.net.http;
  * Created by chenguihang on 2014/6/3.
  */
 public class BasicRequest {
+    public String version;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
 }
